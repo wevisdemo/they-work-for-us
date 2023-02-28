40e7c0d0c5cc275e95df286e72132a5f5f56d10d
MEEP00H <sophonjampasornklin@gmail.com>
2023-02-28 22:23:55 +0700

    add search icon

--- src/components/search/autocomplete.js
+++ src/components/search/autocomplete.js
@@ -50,7 +50,7 @@ const Autocomplete = ({ setIsZoneDialog, setSelected, selected, setZones }) => {
   }
   return (
     <div>
-      <div style={{ display: "flex" }}>
+      <div style={{ display: "flex", position: "relative" }}>
         <Select
           styles={{
             control: baseStyles => ({
@@ -60,6 +60,7 @@ const Autocomplete = ({ setIsZoneDialog, setSelected, selected, setZones }) => {
               border: "3px solid #000000",
               borderRadius: " 4px 0 0 4px",
               textAlign: "start",
+              cursor: "pointer",
             }),
             menu: baseStyles => ({
               ...baseStyles,
@@ -77,6 +78,35 @@ const Autocomplete = ({ setIsZoneDialog, setSelected, selected, setZones }) => {
             Option: customOption,
           }}
         />
+        <div
+          style={{
+            position: "absolute",
+            right: "10px",
+            top: "55%",
+            transform: "translateY(-50%)",
+            pointerEvents: "none",
+          }}
+        >
+          <svg
+            width="30"
+            height="30"
+            viewBox="0 0 30 30"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              d="M19.1368 11.1207C19.1368 15.7381 15.3936 19.4812 10.7763 19.4812C6.15889 19.4812 2.41577 15.7381 2.41577 11.1207C2.41577 6.50337 6.15889 2.76026 10.7763 2.76026C15.3936 2.76026 19.1368 6.50337 19.1368 11.1207Z"
+              stroke="black"
+              stroke-width="4"
+            />
+            <rect
+              width="4"
+              height="16.5821"
+              transform="matrix(0.707107 -0.707106 0.707107 0.707106 15.0305 17.5144)"
+              fill="black"
+            />
+          </svg>
+        </div>
       </div>
     </div>
   )
