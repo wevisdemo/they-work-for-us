bce7ecb19142c4afb647ae9f67620735a0fa1f49
Narat S <me@mixth.net>
2023-03-17 04:10:38 +0700

    feat(data): fetch and generate people.yaml

--- tools/data-fetcher/index.ts
+++ tools/data-fetcher/index.ts
@@ -2,23 +2,29 @@ import fs from "fs"
 import yaml from "js-yaml"
 import jsConvert from "js-convert-case"
 import { NocoDB } from './nocodb'
+import { parsePeople } from './people'
 
 const OUT_DIR = "src/contents"
 const NOCODB_BASE_URL = "https://sheets.wevis.info"
 const NOCODB_TABLE_PARTIES = "40065196-c978-4d7a-b3fb-fb84694383a7"
+const NOCODB_TABLE_PEOPLE = "572c5e5c-a3d8-440f-9a70-3c4c773543ec"
+
+const nocoClient = new NocoDB(NOCODB_BASE_URL)
 
 async function main() {
   if (!fs.existsSync(OUT_DIR)) {
     fs.mkdirSync(OUT_DIR)
   }
 
-  const nocoClient = new NocoDB(NOCODB_BASE_URL)
-
   console.log("Fetching parties...")
-  fs.writeFileSync(`${OUT_DIR}/party.yaml`, yaml.dump(await parseParties(nocoClient)))
+  fs.writeFileSync(`${OUT_DIR}/party.yaml`, yaml.dump(await parseParties()))
+  console.log("Fetching people...")
+  fs.writeFileSync(
+    `${OUT_DIR}/people.yaml`,
+    yaml.dump(await parsePeople(nocoClient, NOCODB_TABLE_PEOPLE)))
 }
 
-async function parseParties(nocoClient: NocoDB) {
+async function parseParties() {
   const parties = await nocoClient.getAllRow(NOCODB_TABLE_PARTIES)
   return parties.map(party => jsConvert.snakeKeys(party))
 }
---
+++ tools/data-fetcher/people/index.ts
@@ -0,0 +1,76 @@
+import jsConvert from 'js-convert-case'
+import { NocoDB } from '../nocodb'
+import { deleteArrayKeys, guardStringToEmpty, parseArray, parseNumber } from '../parsing-utils'
+import { splitPeopleName } from './split-people-name'
+
+export async function parsePeople(nocoClient: NocoDB, viewId: string) {
+  const people = await nocoClient.getAllRow(viewId, {
+		'nested[PeoplePartyHistory][fields]': 'Party,EstablishedDate',
+    'nested[PeoplePartyHistory][nested][Party][fields]': 'Id,Name,PartyGroup',
+	})
+  return people.map(mapPerson)
+}
+
+function mapPerson(person: object) {
+  const converted = jsConvert.snakeKeys(person, { recursive: true, recursiveInArray: true })
+
+  // Strings
+  converted['id'] = guardStringToEmpty(converted['id'])
+  converted['mp_type'] = guardStringToEmpty(converted['mp_type'])
+  converted['mp_province'] = guardStringToEmpty(converted['mp_province'])
+  converted['mp_zone'] = guardStringToEmpty(converted['mp_zone'])
+  converted['mp_list'] = guardStringToEmpty(converted['mp_list'])
+  converted['senator_method'] = guardStringToEmpty(converted['senator_method'])
+  converted['degree'] = guardStringToEmpty(converted['degree'])
+  converted['bio'] = guardStringToEmpty(converted['bio'])
+
+  // Numbers
+  converted['asset'] = parseNumber(converted['asset'])
+  converted['debt'] = parseNumber(converted['debt'])
+  converted['vote'] = parseNumber(converted['vote'])
+
+  // Arrays
+  converted['cabinet_position'] = parseArray<string>('cabinet_position', converted)
+  converted['ex_occupation'] = parseArray<string>('ex_occupation', converted)
+  deleteArrayKeys('cabinet_position', converted)
+  deleteArrayKeys('ex_occupation', converted)
+
+  // Spacial cases
+  // Last name
+  const { name, last_name } = splitPeopleName(converted['name'])
+  converted['name'] = name;
+  converted['lastname'] = last_name;
+  // Party
+  const partyHistory = getLatestPartyHistory(converted['people_party_history'])
+  converted['party'] = partyHistory?.party?.name
+  converted['party_group'] = partyHistory?.party?.party_group
+  // PeopleVotes
+  delete converted['people_votes']
+  
+  return converted
+}
+
+function getLatestPartyHistory(histories: PeoplePartyHistory[]): PeoplePartyHistory {
+  histories.sort(
+    (h1: PeoplePartyHistory, h2: PeoplePartyHistory) => {
+      if (h1.established_date < h2.established_date) {
+        return -1
+      }
+      if (h1.established_date > h2.established_date) {
+        return 1
+      }
+      return 0
+    }
+  )
+
+  return histories[histories.length - 1]
+}
+
+type PeoplePartyHistory = {
+  established_date: string
+  party: {
+    id: number,
+    name: string,
+    party_group: 'ร่วมรัฐบาล' | 'ฝ่ายค้าน' | null,
+  } | null
+}
