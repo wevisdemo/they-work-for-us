9353755d5be6c7536c344514fd57b38bf57cba3a
Narat S <me@mixth.net>
2023-03-18 03:02:34 +0700

    feat(data): enhance parseParties for empty strings and number values

--- tools/data-fetcher/index.ts
+++ tools/data-fetcher/index.ts
@@ -3,6 +3,7 @@ import yaml from "js-yaml"
 import jsConvert from "js-convert-case"
 import { NocoDB } from './nocodb'
 import { parsePeople } from './people'
+import { parseParties } from './parties'
 
 const OUT_DIR = "src/contents"
 const NOCODB_BASE_URL = "https://sheets.wevis.info"
@@ -17,16 +18,16 @@ async function main() {
   }
 
   console.log("Fetching parties...")
-  fs.writeFileSync(`${OUT_DIR}/party.yaml`, yaml.dump(await parseParties()))
+  fs.writeFileSync(
+    `${OUT_DIR}/party.yaml`,
+    yaml.dump(await parseParties(nocoClient, NOCODB_TABLE_PARTIES)),
+  )
+  
   console.log("Fetching people...")
   fs.writeFileSync(
     `${OUT_DIR}/people.yaml`,
-    yaml.dump(await parsePeople(nocoClient, NOCODB_TABLE_PEOPLE)))
-}
-
-async function parseParties() {
-  const parties = await nocoClient.getAllRow(NOCODB_TABLE_PARTIES)
-  return parties.map(party => jsConvert.snakeKeys(party))
+    yaml.dump(await parsePeople(nocoClient, NOCODB_TABLE_PEOPLE))
+  )
 }
 
 main()
---
+++ tools/data-fetcher/parties/index.ts
@@ -0,0 +1,42 @@
+import jsConvert from 'js-convert-case'
+import { NocoDB } from '../nocodb'
+import { guardStringToEmpty, parseNumber } from '../parsing-utils'
+
+export async function parseParties(nocoClient: NocoDB, viewId: string) {
+  const parties = await nocoClient.getAllRow(viewId)
+
+  return parties.map(mapParty)
+}
+
+function mapParty(party: object) {
+  const converted = jsConvert.snakeKeys(party)
+
+  // Strings
+  converted['id'] = guardStringToEmpty(converted['id'])
+  converted['color'] = guardStringToEmpty(converted['color'])
+  converted['party_type'] = guardStringToEmpty(converted['party_type'])
+  converted['party_group'] = guardStringToEmpty(converted['party_group'])
+  converted['dissolved_date'] = guardStringToEmpty(converted['dissolved_date'])
+  converted['speaker'] = guardStringToEmpty(converted['speaker'])
+  converted['first_deputy_speaker'] = guardStringToEmpty(converted['first_deputy_speaker'])
+  converted['second_deputy_speaker'] = guardStringToEmpty(converted['second_deputy_speaker'])
+  converted['whip'] = guardStringToEmpty(converted['whip'])
+  converted['opposition_leader'] = guardStringToEmpty(converted['opposition_leader'])
+  converted['party_leader'] = guardStringToEmpty(converted['party_leader'])
+  converted['party_secretary'] = guardStringToEmpty(converted['party_secretary'])
+  converted['website'] = guardStringToEmpty(converted['website'])
+  converted['facebook'] = guardStringToEmpty(converted['facebook'])
+  converted['twitter'] = guardStringToEmpty(converted['twitter'])
+
+  // Numbers
+  converted['party_ordinal'] = parseNumber(converted['party_ordinal'])
+
+  // Spacial cases
+  // EnName
+  converted['en'] = {
+    name: converted['en_name']
+  }
+  delete converted['en_name']
+  
+  return converted
+}
